964b5bb19e0e772032558b314f2b453187df51c8
omgate <90276220+OMGATE23@users.noreply.github.com>
2022-08-19 19:44:32 +0530

    project 1

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react'
+
+
+function App() {
+  const [number, setNumber] = useState(0)
+
+  function clickHandler(value){
+    if (value == true){
+      setNumber(number + 1)
+    } else if (value == false){
+      setNumber(number - 1)
+    }
+  }
+  return (
+    <div className="App">
+      <div>{number}</div>
+      <button onClick={() => clickHandler(true)}>+</button>
+      <button onClick={() => {clickHandler(false)}}>-</button>
+    </div>
+  );
+}
+
+export default App;
